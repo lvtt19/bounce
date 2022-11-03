e983f9f7847f2620edb31577a595361679fe3562
Vitalii <100052082+lvtt19@users.noreply.github.com>
2022-11-03 22:41:53 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,1 @@
+new Swiper(".swiper-container",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev",clickable:!0},pagination:{el:".swiper-pagination",type:"bullets",clickable:!0},spaceBetween:150,loop:!0});
